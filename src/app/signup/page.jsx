f6739c4af00f4430c383e7f741f0fb90cea7635f
Your Name <you@example.com>
2024-10-27 21:40:45 +0800

    redirected

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -21,7 +21,7 @@ const SignupForm = () => {
         console.log(resp)
         if(resp.status===200){
             e.target.reset()
-            router.push('/todo')
+            router.push('/login')
           
            
           
@@ -30,13 +30,7 @@ const SignupForm = () => {
     return (
         <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-          </div>
+        
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <form onSubmit={handleSignUp} className="card-body">
               <div className="form-control">
