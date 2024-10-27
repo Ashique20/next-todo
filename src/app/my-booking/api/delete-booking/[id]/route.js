c9e918b24e8a46faff7685cef052e93aa943f1ac
Your Name <you@example.com>
2024-10-27 21:46:17 +0800

    post-api

--- src/app/my-booking/api/delete-booking/[id]/route.js
+++ src/app/my-booking/api/delete-booking/[id]/route.js
@@ -19,24 +19,3 @@ export const DELETE = async (request, { params }) => {
   }
 };
 
-// /app/api/my-booking/delete-booking/[id]/route.js
-
-// import { connectDB } from "@/lib/connectDB";
-// import { ObjectId } from "mongodb";
-// import { NextResponse } from "next/server";
-
-// export const DELETE = async (request, { params }) => {
-//   const db = await connectDB();
-//   const bookingCollection = db.collection("fruits");
-
-//   try {
-//     const result = await bookingCollection.deleteOne({ _id: new ObjectId(params.id) });
-//     return NextResponse.json({
-//       message: "Deleted the booking",
-//       response: result, // Make sure to pass the actual delete result here
-//     });
-//   } catch (error) {
-//     console.error("Error deleting booking:", error);
-//     return NextResponse.json({ message: "Something went wrong" });
-//   }
-// };
